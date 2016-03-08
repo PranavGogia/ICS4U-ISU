25e3630e164f712088a4534e083101498d185496
342629581 <342629581@C-DQY1WR1.tdsb.on.ca>
2016-03-08 15:03:40 -0500

    Not done age finder assignment

---
+++ ICS3U/src/gogia/AgeAssignment.java
@@ -0,0 +1,33 @@
+/* Pranav Gogia
+ * 8 March 2016
+ * Age Assignment	
+ * THis program gets your parents birthdays and subtracts your from it to determine 
+ * how old they were when they were born
+ */
+
+package gogia;
+import java.util.*;
+public class AgeAssignment {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner momAgeInput = new Scanner (System.in);
+		Scanner momNameInput = new Scanner(System.in);
+		String fatherName = "";
+		String yourName = "";
+		int fatherAge = 0;
+		int motherAge = 0;
+		int yourAge = 0;
+
+
+		System.out.println("What is your moms birth year");
+		motherAge = Integer.parseInt(momAgeInput.nextLine());
+		System.out.println("When were you born");
+		Scn
+
+
+
+
+	}
+}
