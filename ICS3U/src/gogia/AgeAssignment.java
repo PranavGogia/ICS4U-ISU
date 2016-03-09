1a95109c1acb9fda189f6b0be690b2cbf7196543
342629581 <342629581@C-DQY1WR1.tdsb.on.ca>
2016-03-09 15:00:35 -0500

    Final Copy

--- ICS3U/src/gogia/AgeAssignment.java
+++ ICS3U/src/gogia/AgeAssignment.java
@@ -1,7 +1,7 @@
-/* Pranav Gogia
+/*Pranav Gogia
  * 8 March 2016
  * Age Assignment	
- * THis program gets your parents birthdays and subtracts your from it to determine 
+ * This program gets your parents birthdays and subtracts your from it to determine 
  * how old they were when they were born
  */
 
@@ -12,19 +12,51 @@ public class AgeAssignment {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		Scanner momAgeInput = new Scanner (System.in);
-		Scanner momNameInput = new Scanner(System.in);
-		String fatherName = "";
+
+
+		String dadName = "";
+		String momName = "";
 		String yourName = "";
-		int fatherAge = 0;
+		int dadsAge = 0;
 		int motherAge = 0;
 		int yourAge = 0;
+		int ageMomAndYou = 0;
+		int ageDadAndYou = 0;
 
-
-		System.out.println("What is your moms birth year");
+		System.out.println("What year was your mother born?");
+		Scanner momAgeInput = new Scanner(System.in);
 		motherAge = Integer.parseInt(momAgeInput.nextLine());
-		System.out.println("When were you born");
-		Scn
+
+		System.out.println("What year was your dad born?");
+		Scanner dadsAgeInput = new Scanner(System.in);
+		dadsAge = Integer.parseInt(dadsAgeInput.nextLine());
+
+		System.out.println("What year were you born");
+		Scanner yourAgeInput = new Scanner(System.in);
+		yourAge = Integer.parseInt(yourAgeInput.nextLine());
+
+		System.out.println("What is your moms name?");
+		Scanner momNameInput = new Scanner(System.in);
+		momName = momNameInput.nextLine();
+		
+		System.out.println("What is your dads name?");
+		Scanner dadNameInput = new Scanner(System.in);
+		dadName = momNameInput.nextLine();
+		
+		System.out.println("What is your name?");
+		Scanner yourNameInput = new Scanner(System.in);
+		yourName = momNameInput.nextLine();
+		
+		ageMomAndYou = yourAge - motherAge;
+		ageDadAndYou = yourAge - dadsAge;
+
+		System.out.println("You mom " + momName + " was " + 	ageMomAndYou + " years old when you " + yourName + " was born");
+		System.out.println("You dad " + dadName + " was " + 	ageDadAndYou + " years old when you " + yourName + " was born");
+		
+		
+
+
+
 
 
 
