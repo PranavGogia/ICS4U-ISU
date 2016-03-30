51028e9a25683aad17aa2d9dd7f037bbf03cefd7
342629581 <342629581@C-DQY1WR1.tdsb.on.ca>
2016-03-30 14:16:51 -0400

    While loop exercise

--- ICS3U/src/gogia/DiceRoll.java
+++ ICS3U/src/gogia/DiceRoll.java
@@ -11,65 +11,107 @@ import java.util.Scanner;
 
 public class DiceRoll {
 
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 		// declaring variables
 		String userName = "";
 		int playOrNot = 0;
+		int playAgainOrNot = 0;
 		int playerDice = 0;
 		int playerDice1 = 0;
 		int playerDice2 = 0;
 		int secondDice = 0;
 		int secondDice1 = 0;
 		int secondDice2 = 0;
+
+
+
+		while (playOrNot >= 1);
 		
-		//  get the users name
-		System.out.println("What is your name?");
-		Scanner userNameInput = new Scanner(System.in);
-		userName = userNameInput.nextLine();
-		
-		// assign that the random numbers must be between 1 and 6
-		playerDice1 = (int) (Math.random() * 6 ) + 1;
-		secondDice1 = (int) (Math.random() * 6 ) + 1;
-		
-		playerDice2 = (int) (Math.random() * 6 ) + 1;
-		secondDice2 = (int) (Math.random() * 6 ) + 1;
-		
-		playerDice = playerDice1 + playerDice2;
-		secondDice = secondDice1 + secondDice2;
-		
-		// asking if the user wants to play
-		System.out.println("Are you ready to play? Type in 1 if yes and 0 if no");
-		Scanner playOrNotInput = new Scanner(System.in);
-		playOrNot = Integer.parseInt(playOrNotInput.nextLine());
-		
-		// if user wants to play then the program begins
-		if (playOrNot == 1)
-		{
+
+			// asking if the user wants to play
+			System.out.println("Are you ready to play? Type in 1 for Yes or 0 for No");
+			Scanner playOrNotInput = new Scanner(System.in);
+			playOrNot = Integer.parseInt(playOrNotInput.nextLine());
+
+			//  get the users name
+			System.out.println("What is your name?");
+			Scanner userNameInput = new Scanner(System.in);
+			userName = userNameInput.nextLine();
+
+			// assign that the random numbers must be between 1 and 6
+			playerDice1 = (int) (Math.random() * 6 ) + 1;
+			secondDice1 = (int) (Math.random() * 6 ) + 1;
+
+			playerDice2 = (int) (Math.random() * 6 ) + 1;
+			secondDice2 = (int) (Math.random() * 6 ) + 1;
+
+			playerDice = playerDice1 + playerDice2;
+			secondDice = secondDice1 + secondDice2;
+
+			// if user wants to play then the program begins
+
+
 			System.out.println("Good job, now lets begin");
 			// if the user rolled a higher number than the computer 
 			if (playerDice > secondDice)
 			{
 				System.out.println(" Congratulations " + userName +", you won!!!. You rolled " + playerDice + " while the computer rolled " + secondDice);
+				// asking if the user wants to play
+				System.out.println("Are you ready to play? Type in 1 for Yes or 0 for No");
+				Scanner playAgainOrNotInput = new Scanner(System.in);
+				playAgainOrNot = Integer.parseInt(playAgainOrNotInput.nextLine());
+				if (playAgainOrNot > 0)
+				{
+					playOrNot ++;
+				}
+
+				else
+				{
+					playOrNot = 0;
+				}
 			}
 			// if the user rolled the same number as the computer 
 			else if (playerDice == secondDice)
 			{
+
 				System.out.println("Its a tie! Both you and the computer rolled " + playerDice);
+				// asking if the user wants to play
+				System.out.println("Are you ready to play? Type in 1 for Yes or 0 for No");
+				Scanner playAgainOrNotInput = new Scanner(System.in);
+				playAgainOrNot = Integer.parseInt(playAgainOrNotInput.nextLine());
+				if (playAgainOrNot > 0)
+				{
+					playOrNot ++;
+				}
+
+				else
+				{
+					playOrNot = 0;
+				}
 			}
+
 			// if the user rolled a lower number than the computer 
 			else
 			{
 				System.out.println("Im sorry " + userName + ", You lost :( You rolled " + playerDice + " while the computer rolled " + secondDice);
-			}
-		}
-		// if the user says no then the program stops
-		else 
-		{
-			System.out.println("Im sorry to hear that, maybe next time");
-		}
-	}
-	
+				System.out.println("Are you ready to play? Type in 1 for Yes or 0 for No");
+				Scanner playAgainOrNotInput = new Scanner(System.in);
+				playAgainOrNot = Integer.parseInt(playAgainOrNotInput.nextLine());
+				if (playAgainOrNot > 0)
+				{
+					playOrNot ++;
+				}
 
+				else
+				{
+					playOrNot = 0;
+				}
+			}
+	}
 }
+		
+
+
