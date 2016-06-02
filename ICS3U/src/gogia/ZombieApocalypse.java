24a5a38ca52da46388353c227f53ff3841574d83
342629581 <342629581@C-DQY1WR1.tdsb.on.ca>
2016-06-02 15:02:52 -0400

    First day working on the ISU. Methods started, packages imported, music and audio imported and started work on the main method

---
+++ ICS3U/src/gogia/ZombieApocalypse.java
@@ -0,0 +1,52 @@
+package gogia;
+import java.awt.Color;
+import java.awt.Image;
+import java.awt.Toolkit;
+import hsa_new.Console;
+import java.io.File;
+import java.io.IOException;
+
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+
+@SuppressWarnings("unused")
+public class ZombieApocalypse {
+
+	public static void main(String[] args) throws LineUnavailableException
+	{
+		// TODO Auto-generated method stub
+
+		Console c = new Console();
+		Clip theme = AudioSystem.getClip(); 
+		Clip themewin = AudioSystem.getClip(); 
+		Clip themedie = AudioSystem.getClip(); 
+		Clip themegokuWin = AudioSystem.getClip();
+		Clip themeShower = AudioSystem.getClip();
+		Clip themeBackyard = AudioSystem.getClip();
+		
+		
+	}
+	
+	
+	public static void choseUpstairs()
+	{
+		
+	}
+	
+	public static void choseKitchen()
+	{
+		
+	}
+	
+	public static void died()
+	{
+		
+	}
+	
+	
+	
+	
+
+}
